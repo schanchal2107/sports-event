4b5b1ed79cda3eb55f6dc495d7a938e060dbe26c
schanchal2107 <68811961+schanchal2107@users.noreply.github.com>
2020-11-23 14:14:59 +0530

    Add files via upload

---
+++ CheckAge.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.*;
+class InvalidAgeException extends Exception
+{
+    InvalidAgeException(final String message)
+ {
+       System.out.println("Not for This Time");
+ }
+}
+
+public class CheckAge
+{
+  int age;
+  public static void main (String[] args)
+{
+
+    try
+    {
+	CheckAge obj = new CheckAge();
+       		obj.run (args);
+    }
+    catch (Exception e)
+    {
+        System.out.println("Not Eligible");
+    }
+}
+
+// instance variables here
+
+public void run (String[] args) throws InvalidAgeException
+{
+   Scanner p = new Scanner(System.in);
+   System.out.println("Age:");
+   int age=p.nextInt();
+   if(age<18)
+  throw new InvalidAgeException("not valid");
+  else
+    System.out.println("Eligible");
+
+
+}
+}
